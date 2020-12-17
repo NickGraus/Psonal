a167f706a53ecc013cf2875363cc9aae9213cf5c
ONE SQUAD <n.graus@onesquad.supportgit config --global user.email n.graus@onesquad.support>
2020-12-17 17:31:17 +0100

    styled vacature in overview

--- archive.php
+++ archive.php
@@ -1,20 +1,35 @@
 <?php get_header();?>
 
+<div class="title fixed-top">
+        <div class="container">
+            <h1><?php single_cat_title()?></h1>
+        </div> <!-- end container -->
+    </div> <!-- end title -->
 
-<div class="container pt-5 pb-5">
 
-    <h1><?php single_cat_title()?></h1>
-
+<div class="container pb-5">
 
     <?php if(have_posts()) : while(have_posts()) : the_post();?>
 
     <div class="card mb-4">
+    <h5><?php the_title()?></h5>
         <div class="card-body">
-            <h3><?php the_title()?></h3>
+        <ul class="info">
+            <?php if(get_post_meta($post->ID, 'worktype', true)) {?>
+                <li><?php echo get_post_meta($post->ID, 'worktype', true); ?></li>
+            <?php }?>
+            <?php if(get_post_meta($post->ID, 'salary', true)) {?>
+                <li><?php echo get_post_meta($post->ID, 'salary', true); ?></li>
+            <?php }?>
+            <?php if(get_post_meta($post->ID, 'location', true)) {?>
+                <li><?php echo get_post_meta($post->ID, 'location', true); ?></li>
+            <?php }?>
+            </ul> <!-- end info -->
             <?php the_excerpt();?>
             <a href="<?php the_permalink();?>" class="btn btn-success">Lees meer</a>
-        </div>
-    </div>
+            
+        </div> <!-- end card-body -->
+    </div> <!-- end card -->
 
         
 
--- single.php
+++ single.php
@@ -1,10 +1,12 @@
 <?php get_header();?>
 
+<div class="title fixed-top">
+        <div class="container">
+            <h1><?php the_title()?></h1>
+        </div> <!-- end container -->
+    </div> <!-- end title -->
 
 <div class="container">
-
-    <h1><?php the_title()?></h1>
-
 
     <?php if(have_posts()) : while(have_posts()) : the_post();?>
 
